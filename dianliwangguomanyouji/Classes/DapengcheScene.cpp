12722dbbcbf39048a41996460cbed90aadb43248
shishengchuan <shishengchuan6@sina.com>
2015-09-16 00:40:22 +0800

    第一场就图片逻辑完成 增加了结束标语

---
+++ dianliwangguomanyouji/Classes/DapengcheScene.cpp
@@ -0,0 +1,9 @@
+//
+//  DapengcheScene.cpp
+//  dianliwangguomanyouji
+//
+//  Created by szsycx on 9/15/15.
+//
+//
+
+#include "DapengcheScene.h"
---
+++ dianliwangguomanyouji/Classes/DapengcheScene.h
@@ -0,0 +1,14 @@
+//
+//  DapengcheScene.h
+//  dianliwangguomanyouji
+//
+//  Created by szsycx on 9/15/15.
+//
+//
+
+#ifndef __dianliwangguomanyouji__DapengcheScene__
+#define __dianliwangguomanyouji__DapengcheScene__
+
+#include <stdio.h>
+
+#endif /* defined(__dianliwangguomanyouji__DapengcheScene__) */
